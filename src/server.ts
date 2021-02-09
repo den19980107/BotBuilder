7fe5626b57e18387eb0dfa88b175d9631baf9502
paul_tseng <paul_tseng@gss.com.tw>
2021-02-09 15:11:53 +0800

    🎉 完成連接資料庫功能

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import AuthRoute from './routes/auth';
+import DBConfig from '../config/db.json';
+import connectToDB from './connectToDB';
 
 const app = express();
-
 app.use(express.json());
+
+const dbConnectionString = `mongodb://${DBConfig.HOST}:${DBConfig.PORT}/${DBConfig.DBNAME}`
+connectToDB(dbConnectionString)
 
 // register routes
 new AuthRoute(app);
-
-
 
 app.listen(5000, () => {
     console.log(`app listening at http://localhost:5000`)
