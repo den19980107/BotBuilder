dc10c5d46908f0d5bfc3a8776cd9adb504641c13
den19980107 <den19980107@gmail.com>
2021-03-16 21:48:17 +0800

    解決 declar variable node 沒有 export 的問題

--- src/converter/nodes/declarVariable.node.ts
+++ src/converter/nodes/declarVariable.node.ts
@@ -2,7 +2,7 @@ import globalVariable from "../helper/globalVariable";
 import ScriptParser from "../helper/scriptParser";
 import node from "./INode";
 
-class DeclarVariable extends node {
+export default class DeclarVariable extends node {
     payload: { key: string, value: any }
 
     constructor(id: string, name: string, payload: any, type: string, next_node_id: string | null) {
@@ -11,6 +11,9 @@ class DeclarVariable extends node {
     }
 
     async run(param: any): Promise<void> {
+        console.log("declar variable node is check if need parseing")
+        this.checkIfNeedParsing(this.payload);
+        console.log("declar variable node is running...")
         globalVariable.set(this.payload.key, this.payload.value);
     }
 
