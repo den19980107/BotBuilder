5506d90dfc7f47963e06a4f201da921e9cb7cf23
den19980107 <den19980107@gmail.com>
2021-03-16 16:18:14 +0800

    加入常用的 constants

---
+++ src/converter/constant/httpMethod.constants.ts
@@ -0,0 +1,9 @@
+export const OPTIONS = "OPTIONS"
+export const HEAD = "HEAD"
+export const GET = "GET"
+export const POST = "POST"
+export const PUT = "PUT"
+export const DELETE = "DELETE"
+export const TRACE = "TRACE"
+export const CONNECT = "CONNECT"
+
---
+++ src/converter/constant/nodeType.constants.ts
@@ -0,0 +1,8 @@
+export const WEB_HOOK = "WEB_HOOK";
+export const FETCH_DATA = "FETCH_DATA";
+export const DECLAR_VARIABLE = "DECLAR_VARIABLE";
+export const CONDITION = "CONDITION";
+export const LOOP = "LOOP";
+export const BASIC_CACULATION = "BASIC_CACULATION";
+export const HTTP_RESPONSE = "HTTP_RESPONSE";
+
